abdc7695d82ac703592b9f906fb8792de3484ab2
Shivank12batra <Shivank56batra@gmail.com>
2022-04-01 03:44:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let button = document.getElementById('translate-btn');
+let text = document.getElementById('text-area');
+let output = document.getElementById('output');
+
+let url = 'https://api.funtranslations.com/translate/minion.json'
+
+function urlConcatenate(text) {
+    return url + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log('error occured', error)
+    alert('something wrong with the server! please try again after some time!')
+}
+
+function clickHandler() {
+    var inputText = text.value
+
+    // calling the server for processing
+    fetch(urlConcatenate(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var outputText = json.contents.translated;
+        output.innerText = outputText;
+    })
+    .catch(errorHandler)
+}
+
+button.addEventListener('click', clickHandler)
